95b2bfb13728461e7ae29e2c056c147fac1cf722
harsh-2312 <153928215+harsh-2312@users.noreply.github.com>
2026-06-27 11:55:25 +0530

    Refactor prisma client initialization to use globalThisg

--- apps/api/src/utils/prisma.ts
+++ apps/api/src/utils/prisma.ts
@@ -4,5 +4,5 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+export const prisma = globalThis.prisma ?? new PrismaClient();
+globalThis.prisma = prisma;
